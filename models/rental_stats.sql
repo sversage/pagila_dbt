8143095367e25283d0a2b15bcb4ffa4bff825176
Marc Versage <skylar.versage@gmail.com>
2021-07-07 09:41:17 -0700

    update sql

--- models/rental_stats.sql
+++ models/rental_stats.sql
@@ -16,35 +16,40 @@ WITH enriched_data as (
         date_part('week', to_date(r.return_date, 'YYYY/MM/DD')) AS return_week,
         date_part('year', to_date(r.return_date, 'YYYY/MM/DD')) AS return_year
     FROM rental r 
-), rental_stats as (
+), 
+weekly_return_count as (
+	    SELECT date_trunc('week', return_date::timestamp)::date AS WeekBeginning, count(*) AS ReturnedRentals
+	    FROM enriched_data er 
+	    WHERE rental_week = return_week and rental_year = return_year 
+	    GROUP BY date_trunc('week', return_date::timestamp)::date
+	UNION ALL
+	    SELECT date_trunc('week', return_date::timestamp)::date AS WeekBeginning, count(*) AS ReturnedRentals
+	    FROM enriched_data er 
+	    WHERE rental_week != return_week
+	    GROUP BY date_trunc('week', return_date::timestamp)::date
+), 
+aggregate_returns AS (
+    SELECT WeekBeginning, SUM(ReturnedRentals) AS ReturnedRentals
+    FROM weekly_return_count
+    GROUP BY WeekBeginning
+), 
+aggregate_outstanding_rentals AS (
+    SELECT date_trunc('week', rental_date ::timestamp)::date AS WeekBeginning, count(*) AS OutstandingRentals
+    FROM enriched_data er 
+    WHERE rental_week != return_week or return_week ISNULL
+    GROUP BY date_trunc('week', rental_date ::timestamp)::date
+), 
+rental_stats as (
     SELECT 
         CASE 
-            WHEN agg_returns.WeekBeginning ISNULL THEN open_rentals.WeekBeginning
-            ELSE agg_returns.WeekBeginning
+            WHEN aggregate_returns.WeekBeginning ISNULL THEN open_rentals.WeekBeginning
+            ELSE aggregate_returns.WeekBeginning
         END as WeekBeginning,
-        agg_returns.ReturnedRentals,
+        aggregate_returns.ReturnedRentals,
         open_rentals.OutstandingRentals
 
-    FROM (
-        SELECT returned_info.WeekBeginning, SUM(returned_info.ReturnedRentals) AS ReturnedRentals
-        FROM(
-                SELECT date_trunc('week', return_date::timestamp)::date AS WeekBeginning, count(*) AS ReturnedRentals
-                FROM enriched_data er 
-                WHERE rental_week = return_week and rental_year = return_year 
-                GROUP BY date_trunc('week', return_date::timestamp)::date
-            UNION ALL
-                SELECT date_trunc('week', return_date::timestamp)::date AS WeekBeginning, count(*) AS ReturnedRentals
-                FROM enriched_data er 
-                WHERE rental_week != return_week
-                GROUP BY date_trunc('week', return_date::timestamp)::date
-                ) returned_info
-        GROUP BY returned_info.WeekBeginning ) AS agg_returns
-    FULL JOIN (
-        SELECT date_trunc('week', rental_date ::timestamp)::date AS WeekBeginning, count(*) AS OutstandingRentals
-        FROM enriched_data er 
-        WHERE rental_week != return_week or return_week ISNULL
-        GROUP BY date_trunc('week', rental_date ::timestamp)::date
-    ) AS open_rentals ON open_rentals.WeekBeginning = agg_returns.WeekBeginning
+    FROM aggregate_returns 
+    FULL JOIN aggregate_outstanding_rentals AS open_rentals ON open_rentals.WeekBeginning = aggregate_returns.WeekBeginning
     ORDER BY WeekBeginning
 )
 
